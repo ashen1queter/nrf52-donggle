2aa5fef1276ec661476d422357cc749e1f297015
ashen1queter <mnojasish@gmail.com>
2025-06-24 19:51:05 +0530

    Not done: Whitelist and Scan filters

---
+++ pika/nRF5_SDK_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/examples/jesica/ble_app_blinky/pca10059/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf52840.o: \
+ C:\Users\HI\Desktop\pika\nRF5_SDK_17.1.0_ddde560\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf52840.s
---
+++ pika/nRF5_SDK_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/examples/jesica/ble_app_blinky/pca10059/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf_common.o: \
+ C:\Users\HI\Desktop\pika\nRF5_SDK_17.1.0_ddde560\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
